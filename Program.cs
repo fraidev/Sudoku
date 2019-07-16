77e19966e7353a5872703dbdc2d57b769ad7a615
Felipe Cardozo <felipe.cardozo@sapiensia.com>
2019-07-16 00:09:41 -0300

    this

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Sudoku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var sudoku = new Sudoku();
+
+            
+            for (int z = 0; z < 9; z++)
+            {
+                for (var y = 0; y < 3; y++)
+                {
+                    for (var x = 0; x < 3; x++)
+                    {
+                        sudoku.Board[x,y,z] = z;
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            for (int y = 0; y < 9; y++)
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    Console.Write(sudoku.BoardView[x, y] + "|");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Sudoku.cs
@@ -0,0 +1,100 @@
+﻿namespace Sudoku
+{
+    class Sudoku
+    {
+        public int[,,] Board { get; set; } = new int[3, 3, 9];
+
+        public int[,] BoardView
+        {
+            get
+            {
+                var m = new int[9, 9];
+
+                for (int y = 0; y < 9; y++)
+                {
+                    for (int x = 0; x < 9; x++)
+                    {
+                        var nx = x % 3;
+
+                        var ny = y % 3;
+
+
+                        var nz = getZ(nx, ny);
+
+                        m[x, y] = Board[nx, ny, nz - 1];
+                    }
+                }
+
+
+                return m;
+            }
+        }
+
+        private int getZ(int x, int y)
+        {
+            x += 1;
+            y += 1;
+            if (x == 1)
+            {
+                if (y == 1)
+                {
+                    return 1;
+                }
+
+                if (y == 2)
+                {
+                    return 2;
+                }
+
+                if (y == 3)
+                {
+                    return 3;
+                }
+            }
+            if (x == 2)
+            {
+
+                if (y == 1)
+                {
+                    return 4;
+                }
+
+                if (y == 2)
+                {
+                    return 5;
+                }
+
+                if (y == 3)
+                {
+                    return 6;
+                }
+
+            }
+
+            if (x == 3)
+            {
+
+                if (y == 1)
+                {
+                    return 7;
+                }
+
+                if (y == 2)
+                {
+                    return 8;
+                }
+
+                if (y == 3)
+                {
+                    return 9;
+                }
+            }
+
+            throw new System.Exception();
+        }
+
+        public Sudoku()
+        {
+        }
+    }
+}
